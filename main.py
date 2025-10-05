5804300bcdad125a9703ce55dca06b4c6df03727
Tariq Sohail <tariqsohail1110@gmail.com>
2025-10-05 23:52:11 +0500

    Initial Chatbot Prepared

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import streamlit as st
-import backend
+import backend as bk
 
-st.set_page_config(page_title = "WhsiperPDF", page_icon = ":green_book:", layout = "centered")
+st.set_page_config(page_title = "CasperAI", page_icon = ":books:", layout = "centered")
 
 
-st.title("WhisperPDF")
+st.title("CasperAI")
 
-st.file_uploader(label = "", type = "PDF")
+pdf_file = st.file_uploader(label = "", type = ["pdf"])
+user_prompt = st.chat_input(placeholder = "Ask Casper")
 
-st.text_area(label = "", placeholder = "Enter Your Prompt")
-
-st.button("Ask Whisper")
+if pdf_file and user_prompt:
+    message = st.chat_message("user")
+    response = st.chat_message("assistant")
+    message.write(user_prompt)
+    text = bk.extractPdf(pdf_file)
+    # st.session_state["pdf_text": text]
+    response.write(bk.Q_and_A_Chatbot(text, user_prompt))
